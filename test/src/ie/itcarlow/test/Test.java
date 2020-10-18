7c1c5228885bc12c60d64df01a2e61a10b1b896c
samcu <samcu@DESKTOP-0S3B5JO.home>
2020-10-18 23:51:01 +0100

    Added third string

--- test/src/ie/itcarlow/test/Test.java
+++ test/src/ie/itcarlow/test/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("This is a test");
 		System.out.println("Added second print string using Github");
+		System.out.println("Added third print string");
 	}
 
 }
