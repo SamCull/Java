55d3ae2f9375e401efb89d224b9292b26b56395e
samcu <samcu@DESKTOP-0S3B5JO.home>
2020-10-18 23:32:44 +0100

    Created initial file

---
+++ test/src/ie/itcarlow/test/Test.java
@@ -0,0 +1,10 @@
+package ie.itcarlow.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test");
+	}
+
+}
