f901e744af028e7e581650e4330c5a1ad0823493
samcu <samcu@DESKTOP-0S3B5JO.home>
2020-10-19 01:08:37 +0100

    Added second Constructor

--- test/src/week2/lab1/ThermTest.java
+++ test/src/week2/lab1/ThermTest.java
@@ -16,5 +16,10 @@ public class ThermTest
 		thermA.setCelsius(20.0);
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
 		
+		Thermometer thermB = new Thermometer(10.0);
+		
+		double tempB = thermB.getCelsius();
+		
+		System.out.println("Temp of Thermometer B is " + tempB);
 	} // end main
 } // end class ThermTest
