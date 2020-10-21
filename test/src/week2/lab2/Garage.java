4d1e3ec227e440ab57bb847d3313c4b497f56f62
samcu <samcu@DESKTOP-0S3B5JO.home>
2020-10-21 13:48:48 +0100

    Creating choices for user

---
+++ test/src/week2/lab2/Garage.java
@@ -0,0 +1,79 @@
+package week2.lab2;
+
+import java.util.Scanner;
+
+public class Garage {
+
+	public static void main(String[] args) {
+		
+		
+		int choice=0;
+		int carTotal=0;
+		Car[] cars = new Car[10];
+		
+		while(choice !=4) {
+
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter a choice:");
+		System.out.println("1.List all cars");
+		System.out.println("2.Enter a car for sale");
+		System.out.println("3.List all cats being sold");
+		System.out.println("4.Exit");
+		
+		choice = in.nextInt();
+		
+		if(choice == 1) {
+			for(Car c: cars) {
+				if (c != null)
+				System.out.println(c.toString() );
+				}		
+		}
+		
+		else if (choice == 2){
+			if (carTotal <9) {
+			Scanner incar = new Scanner(System.in);
+			String make;
+			String model;
+			double engineSize;
+			double price;
+			
+			System.out.println("Enter car make");
+			make = incar.nextLine();
+			
+			System.out.println("Enter car model");
+			model = incar.nextLine();
+			
+			System.out.println("Enter car engine size");
+			engineSize = incar.nextDouble();
+			
+			System.out.println("Enter car price");
+			price = incar.nextDouble();
+			
+			Car c = new Car(make,model,engineSize,price);
+			
+			cars [carTotal] = c;
+			carTotal++; //increments loop by 1
+		}
+			else {
+				System.out.println("You have too many cars");
+			}
+		}
+		
+		
+		else if (choice == 3){
+		System.out.println("Choice 3");
+		}
+		
+		
+		else if (choice == 4){
+		System.out.println("Goodbye");
+		System.exit(0);
+		}
+		
+		}//end while
+		
+	} //end main
+
+}//end class
