6ebd1e4d3ede6a1a64feb5648d3f5f66d9cc3316
samcu <samcu@DESKTOP-0S3B5JO.home>
2020-10-23 02:13:40 +0100

    Created initial class Rectangle

---
+++ test/src/week2/lab2b/Rectangle.java
@@ -0,0 +1,54 @@
+package week2.lab2b;
+
+public class Rectangle {
+	
+	//Variables
+	private float length;
+	private float width;
+	//Constructors
+	
+	public Rectangle() {
+		setLength(1.0f);
+		setWidth(1.0f);
+	}
+	
+	//Getters & Setters 
+	public float getLength() {
+		return length;
+	}
+	public void setLength(float length) {
+		if(length > 0.0 && length <= 40.0) {
+		this.length = length;
+	}
+		else {
+			System.out.println("Length must be between 0.0 and 40.0");
+		}
+	}
+	
+	public float getWidth() {
+		return width;
+	}
+	
+	public void setWidth(float width) {
+		if(width > 0.0 && length <= 40.0) {
+		this.width = width;
+	}
+		else {
+			System.out.println("Width must be between 0.0 and 40.0");
+		}
+	}
+
+
+	
+	//toString	
+		@Override
+	public String toString() {
+		return "Rectangle [length=" + length + ", width=" + width + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
